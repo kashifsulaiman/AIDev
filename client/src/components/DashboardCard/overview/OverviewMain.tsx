28c3daba47761a164afebd682effbc9ff5e988b4
Mohammad Kashif Sulaiman <m.kqagraphix@gmail.com>
2025-01-09 15:50:46 +0500

    fix: build issues

--- client/src/components/DashboardCard/overview/OverviewMain.tsx
+++ client/src/components/DashboardCard/overview/OverviewMain.tsx
@@ -19,9 +19,12 @@ const OverviewMain = () => {
   const extractAttributes = (inputPrompt: string) => {
     const allowedFrameworks = ['next', 'react', 'vue'];
     const lowerCasePrompt = inputPrompt.toLowerCase();
-    const attributes = { framework: 'react'};
-    const findFrameworkIndex = allowedFrameworks.findIndex(item => lowerCasePrompt.includes(item));
-    if(findFrameworkIndex !== -1) attributes.framework = allowedFrameworks[findFrameworkIndex]
+    const attributes = { framework: 'react' };
+    const findFrameworkIndex = allowedFrameworks.findIndex((item) =>
+      lowerCasePrompt.includes(item)
+    );
+    if (findFrameworkIndex !== -1)
+      attributes.framework = allowedFrameworks[findFrameworkIndex];
     return attributes;
   };
   const promtHandler = async (promptData: any) => {
