1dd0c1fbcce4e929ee2c1103b24cca65c312b1ca
Syed-Abdullah-Expertizo <abdullah.naseer@expertizo.pk>
2025-01-16 19:22:58 +0500

    fix: add preview page on new tab

--- client/src/app/(dashboard)/preview/page.tsx
+++ client/src/app/(dashboard)/preview/page.tsx
@@ -1,149 +1,39 @@
 'use client';
 
-// import { Code } from '@/components/code-editor';
-// import Sidebar from '@/components/code-editor/sidebar';
-import React, { Suspense, useEffect, useState } from 'react';
-import Loader from '@/Loader/loading';
-import { useStoreState } from 'easy-peasy';
-// import { getIcon } from '@/components/code-editor/icon';
-import {
-  SandpackProvider,
-  SandpackLayout,
-  SandpackFileExplorer,
-  SandpackCodeEditor,
-  SandpackPreview,
-} from '@codesandbox/sandpack-react';
-
-// function generateFileName(componentCode: string) {
-//   // Match the component name after `const` or `function` keywords
-//   const match = componentCode?.match(/(?:const|function)\s+([A-Za-z0-9_]+)/);
-
-//   if (match && match[1]) {
-//     return `${match[1]}.jsx`;
-//   }
-
-//   return 'Component.jsx';
-// }
+import StackBlitzSDK from '@stackblitz/sdk';
+import React, { useEffect } from 'react';
 
 function Page() {
-  const [isLoading, setIsLoading] = useState(true);
-  const prompt = useStoreState((state: any) => state?.promptModel?.prompt);
-  const jsxCode = prompt?.code;
-  // const cssCode = `.hello {
-  //   color: red;
-  //   font-size: 24px;
-  // }`;
 
   useEffect(() => {
-    if (prompt?.code) {
-      setIsLoading(false);
+    let LSD = localStorage.getItem('proj-code')
+    if (!LSD) { return }
+    let data = JSON.parse(LSD)
+    console.log(typeof data)
+    if (typeof data === 'object') {
+      StackBlitzSDK.embedProject('embed', data, {
+        height: 600,
+        openFile: 'package.json',
+        terminalHeight: 50,
+        startScript: 'dev',
+        hideDevTools: true,
+        view: 'preview',
+        theme: 'light',
+        hideNavigation: true,
+        hideExplorer: true,
+        showSidebar: false
+      });
     }
-  }, [prompt?.code]);
-  // const icon = getIcon('jsx', 'nameabc');
+  }, [])
 
   return (
-    <>
-      {isLoading ? (
-        <div className="h-full bg-opacity-25 bg-purple-white-gradient-opactity25 lg:h-screen">
-          <div className="flex size-full items-center justify-center">
-            <div style={{ height: '40px', width: '40px' }} className="size-10">
-              <Loader Color="#961CBE" />
-            </div>
-          </div>
-        </div>
-      ) : (
-        <>
-          <div className="h-full bg-opacity-25 bg-purple-white-gradient-opactity25 lg:h-screen">
-            <div className="flex h-full w-full">
-              {/* <Sidebar>
-                <div
-                  className={`flex items-center ${true ? 'bg-[#6f63c5] hover:!bg-none' : 'bg-transparent hover:bg-[#b9b2e9]'} mb-0.5 hover:text-white`}
-                >
-                  <span className="flex h-8 w-8 items-center justify-center">
-                    {icon}
-                  </span>
-
-                  <span className="ml-[1px] !text-black">
-                    {generateFileName(prompt?.code)}
-                  </span>
-                </div>
-                <div
-                  className={`flex items-center ${true ? 'bg-[#6f63c5] hover:!bg-none' : 'bg-transparent hover:bg-[#b9b2e9]'} mb-0.5 hover:text-white`}
-                >
-                  <span className="flex h-8 w-8 items-center justify-center">
-                    {icon}
-                  </span>
-
-                  <span className="ml-[1px] !text-black">
-                    Chatbot.css
-                  </span>
-                </div>
-              </Sidebar> */}
-              {/* <Code code={jsxCode} type='javascript' onChange={(value) => setJsxCode(value || "")} />
-              <Code code={cssCode} type='css' onChange={(value) => setCssCode(value || "")} /> */}
-              <SandpackProvider
-                template="react"
-                options={{
-                  externalResources: ['https://cdn.tailwindcss.com'],
-                }}
-                style={{
-                  height: '100%',
-                  width: '100%',
-                }}
-                files={jsxCode}
-              >
-                <SandpackLayout style={{ height: '100%' }}>
-                  <SandpackFileExplorer style={{ height: '100%' }} />
-                  <SandpackCodeEditor
-                    showTabs
-                    showLineNumbers={false}
-                    showInlineErrors
-                    wrapContent
-                    closableTabs
-                    style={{ height: '100%' }}
-                  />
-                  <SandpackPreview style={{ height: '100%' }} />
-                </SandpackLayout>
-              </SandpackProvider>
-            </div>
-
-            {/* Live Preview */}
-
-            {/* <div
-              style={{
-                flex: 1,
-                border: "1px solid #ddd",
-                padding: "10px",
-                overflow: "auto",
-                width: 300
-              }}
-            >
-              <style>{cssCode}</style>
-              <LiveProvider
-                language="jsx"
-                code={jsxCode}
-                noInline
-                scope={{ React, useState, useEffect, useRef }}
-              >
-                <LivePreview />
-              </LiveProvider>
-            </div> */}
-          </div>
-        </>
-      )}
-    </>
+    <div className="h-screen w-full bg-transparent relative overflow-y-hidden" >
+      <div className='w-full' style={{height:`calc(100vh + 32px)`}}>
+        <div className="w-full h-full" id="embed"></div>
+      </div>
+    </div>
   );
+
 }
 
-const PageWithSuspense = () => (
-  <Suspense
-    fallback={
-      <div className="flex h-full w-full items-center justify-center">
-        <Loader Color="#961CBE" />
-      </div>
-    }
-  >
-    <Page />
-  </Suspense>
-);
-export default PageWithSuspense;
+export default Page;
--- client/src/components/DashboardCard/overview/OverviewRight.tsx
+++ client/src/components/DashboardCard/overview/OverviewRight.tsx
@@ -4,8 +4,13 @@ import Loader from '@/Loader/loading';
 import 'prismjs/themes/prism-dark.css';
 import StackBlitzSDK from '@stackblitz/sdk';
 import { useEffect } from 'react';
+import { VscPreview } from "react-icons/vsc";
+import { useRouter } from 'next/navigation';
 
 const OverviewRight = ({ code, loader }: any) => {
+
+  const router = useRouter()
+
   useEffect(() => {
     if (code) {
       StackBlitzSDK.embedProject('embed', code, {
@@ -13,15 +18,36 @@ const OverviewRight = ({ code, loader }: any) => {
         openFile: 'package.json',
         terminalHeight: 50,
         startScript: 'dev',
-        hideDevTools: false,
-        view: 'editor',
+        hideDevTools: true,
+        view: 'preview',
         theme: 'light',
+        hideNavigation: true,
       });
     }
   }, [code]);
 
+  const getCodeAndRedirect = async () => {
+    const iframe = document.getElementById('embed') as HTMLIFrameElement;
+    const vm = await StackBlitzSDK.connect(iframe);
+    const fsSS = await vm.getFsSnapshot();
+    let newCode = code
+    newCode.files = fsSS
+    localStorage.setItem('proj-code', JSON.stringify(newCode))
+    const relativeUrl = `${window.location.origin}/preview`;
+    console.log(relativeUrl)
+    window.open(relativeUrl, '_blank');
+  }
+
   return (
-    <div className="relative flex h-screen w-full items-center justify-center">
+    <div className="relative flex flex-col h-screen w-full items-end justify-center">
+      <div className="mb-2 flex items-end justify-end gap-4 pt-10 md:mb-0 md:justify-end md:pt-0 p-2">
+        <div className="flex h-10 w-12 items-center justify-center rounded bg-custom-gradient p-2">
+          <button className="text-2xl text-white" onClick={getCodeAndRedirect}>
+            <VscPreview />
+          </button>
+        </div>
+      </div>
+
       {loader ? (
         <div className="flex size-full items-center justify-center">
           <div className="size-10">
