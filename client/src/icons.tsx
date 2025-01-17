887eae0565345264881cee102dbff9a5d341244a
Syed-Abdullah-Expertizo <abdullah.naseer@expertizo.pk>
2025-01-17 12:06:02 +0500

    fix: prettier errors

--- client/src/icons.tsx
+++ client/src/icons.tsx
@@ -2,7 +2,5 @@ import { VscPreview } from 'react-icons/vsc';
 import { IconInterface } from './interface';
 
 export const CodeIcon = ({ classes }: IconInterface) => {
-    return (
-        <VscPreview className={classes} />
-    )
-}
+  return <VscPreview className={classes} />;
+};
--- client/src/interface.tsx
+++ client/src/interface.tsx
@@ -1,3 +1,3 @@
 export interface IconInterface {
-    classes?: string
+  classes?: string;
 }
