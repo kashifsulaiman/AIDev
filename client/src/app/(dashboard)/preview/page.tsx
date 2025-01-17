6877d1b01a21f7e0b70b8b607c213b6eb9bf1558
Syed-Abdullah-Expertizo <abdullah.naseer@expertizo.pk>
2025-01-17 11:38:46 +0500

    fix: clean up code formatting and improve readability in dashboard components

--- client/src/app/(dashboard)/preview/page.tsx
+++ client/src/app/(dashboard)/preview/page.tsx
@@ -4,12 +4,13 @@ import StackBlitzSDK from '@stackblitz/sdk';
 import React, { useEffect } from 'react';
 
 function Page() {
-
   useEffect(() => {
-    let LSD = localStorage.getItem('proj-code')
-    if (!LSD) { return }
-    let data = JSON.parse(LSD)
-    console.log(typeof data)
+    const LSD = localStorage.getItem('proj-code');
+    if (!LSD) {
+      return;
+    }
+    const data = JSON.parse(LSD);
+    console.log(typeof data);
     if (typeof data === 'object') {
       StackBlitzSDK.embedProject('embed', data, {
         height: 600,
@@ -21,19 +22,18 @@ function Page() {
         theme: 'light',
         hideNavigation: true,
         hideExplorer: true,
-        showSidebar: false
+        showSidebar: false,
       });
     }
-  }, [])
+  }, []);
 
   return (
-    <div className="h-screen w-full bg-transparent relative overflow-y-hidden" >
-      <div className='w-full' style={{height:`calc(100vh + 32px)`}}>
-        <div className="w-full h-full" id="embed"></div>
+    <div className="relative h-screen w-full overflow-y-hidden bg-transparent">
+      <div className="w-full" style={{ height: `calc(100vh + 32px)` }}>
+        <div className="h-full w-full" id="embed"></div>
       </div>
     </div>
   );
-
 }
 
 export default Page;
--- client/src/components/DashboardCard/overview/OverviewRight.tsx
+++ client/src/components/DashboardCard/overview/OverviewRight.tsx
@@ -4,13 +4,9 @@ import Loader from '@/Loader/loading';
 import 'prismjs/themes/prism-dark.css';
 import StackBlitzSDK from '@stackblitz/sdk';
 import { useEffect } from 'react';
-import { VscPreview } from "react-icons/vsc";
-import { useRouter } from 'next/navigation';
+import { VscPreview } from 'react-icons/vsc';
 
 const OverviewRight = ({ code, loader }: any) => {
-
-  const router = useRouter()
-
   useEffect(() => {
     if (code) {
       StackBlitzSDK.embedProject('embed', code, {
@@ -30,17 +26,17 @@ const OverviewRight = ({ code, loader }: any) => {
     const iframe = document.getElementById('embed') as HTMLIFrameElement;
     const vm = await StackBlitzSDK.connect(iframe);
     const fsSS = await vm.getFsSnapshot();
-    let newCode = code
-    newCode.files = fsSS
-    localStorage.setItem('proj-code', JSON.stringify(newCode))
+    const newCode = code;
+    newCode.files = fsSS;
+    localStorage.setItem('proj-code', JSON.stringify(newCode));
     const relativeUrl = `${window.location.origin}/preview`;
-    console.log(relativeUrl)
+    console.log(relativeUrl);
     window.open(relativeUrl, '_blank');
-  }
+  };
 
   return (
-    <div className="relative flex flex-col h-screen w-full items-end justify-center">
-      <div className="mb-2 flex items-end justify-end gap-4 pt-10 md:mb-0 md:justify-end md:pt-0 p-2">
+    <div className="relative flex h-screen w-full flex-col items-end justify-center">
+      <div className="mb-2 flex items-end justify-end gap-4 p-2 pt-10 md:mb-0 md:justify-end md:pt-0">
         <div className="flex h-10 w-12 items-center justify-center rounded bg-custom-gradient p-2">
           <button className="text-2xl text-white" onClick={getCodeAndRedirect}>
             <VscPreview />
