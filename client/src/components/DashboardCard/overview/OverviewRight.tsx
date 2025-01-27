bf0cbe10745908e0f6cba25e783733a47cf5d3fd
Syed-Abdullah-Expertizo <abdullah.naseer@expertizo.pk>
2025-01-27 19:03:58 +0500

    fix: code button

--- client/src/components/DashboardCard/overview/OverviewRight.tsx
+++ client/src/components/DashboardCard/overview/OverviewRight.tsx
@@ -30,29 +30,7 @@ const OverviewRight = ({
 
   return (
     <div className="relative flex h-full min-h-screen w-full flex-col items-center justify-center overflow-hidden">
-      <div className="absolute right-6 top-4 z-10 flex items-end justify-end gap-4 rounded-lg border-4 border-white">
-        <div className="group relative flex size-8 items-center justify-center rounded bg-custom-gradient p-2">
-          {view ? (
-            <button
-              className="text-2xl text-white"
-              onClick={() => togglePreview('default')}
-            >
-              <CodeIcon />
-            </button>
-          ) : (
-            <button
-              className="text-2xl text-white"
-              onClick={() => togglePreview('preview')}
-            >
-              <BackArrowIcon />
-            </button>
-          )}
 
-          <div className="absolute left-1/2 top-full mt-2 w-max -translate-x-1/2 scale-0 transform rounded bg-gray-800 p-1 text-xs text-white opacity-0 transition-all duration-300 group-hover:scale-100 group-hover:opacity-100">
-            {view ? `Code` : `Back`}
-          </div>
-        </div>
-      </div>
 
       {loader ? (
         <div className="flex size-full items-center justify-center">
@@ -60,7 +38,30 @@ const OverviewRight = ({
             <Loader Color="#961CBE" />
           </div>
         </div>
-      ) : (
+      ) : (<>
+        <div className="absolute right-6 top-4 z-10 flex items-end justify-end gap-4 rounded-lg border-4 border-white">
+          <div className="group relative flex size-8 items-center justify-center rounded bg-custom-gradient p-2">
+            {view ? (
+              <button
+                className="text-2xl text-white"
+                onClick={() => togglePreview('default')}
+              >
+                <CodeIcon />
+              </button>
+            ) : (
+              <button
+                className="text-2xl text-white"
+                onClick={() => togglePreview('preview')}
+              >
+                <BackArrowIcon />
+              </button>
+            )}
+
+            <div className="absolute left-1/2 top-full mt-2 w-max -translate-x-1/2 scale-0 transform rounded bg-gray-800 p-1 text-xs text-white opacity-0 transition-all duration-300 group-hover:scale-100 group-hover:opacity-100">
+              {view ? `Code` : `Back`}
+            </div>
+          </div>
+        </div>
         <div
           className="relative w-full bg-black"
           style={{
@@ -69,6 +70,7 @@ const OverviewRight = ({
         >
           <div className="h-full w-full" id="embed"></div>
         </div>
+      </>
       )}
     </div>
   );
