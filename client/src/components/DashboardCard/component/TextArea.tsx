8f243cb66dd542bd883f4935eecc129ee2ee9193
Mohammad Kashif Sulaiman <m.kqagraphix@gmail.com>
2025-02-21 15:59:26 +0500

    Revert "Fix: Input field set to empty issue"

--- client/src/components/DashboardCard/component/TextArea.tsx
+++ client/src/components/DashboardCard/component/TextArea.tsx
@@ -1,24 +1,24 @@
 'use client';
 
-import React, { useState, useEffect, useRef } from 'react';
-import { Button } from '@nextui-org/react';
+import React, { useState, useEffect } from 'react';
+import { Textarea, Button } from '@nextui-org/react';
 import { Addicon } from '@/components/SVG';
 import { useStoreActions, useStoreState } from 'easy-peasy';
-import { useRouter, usePathname } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 import { ApiUrl } from '@/constants/apiUrl';
 import { POST } from '@/hooks/consts';
 import { useMutation } from '@/hooks/useMutation';
 import { extractAttributes } from '@/utils/utils';
 import Loader from '@/Loader/loading';
 import { StoreModel } from '@/redux/model';
-import { useDebounce } from '@/hooks/useDebounce';
 
 const TextArea = ({
   prompt,
+  classNames,
   // isExpanded,
+  ...props
 }: any) => {
   const router = useRouter();
-  const pathname = usePathname();
   const promptData = useStoreState<StoreModel>(
     (state) => state?.promptModel?.prompt
   );
@@ -26,55 +26,17 @@ const TextArea = ({
     (state) => state?.conversationModel?.conversation
   );
   const user = useStoreState<StoreModel>((state) => state?.userObj?.UserObj);
-  const { setPrompt } = useStoreActions<StoreModel>(
-    (actions) => actions?.promptModel
+  const setPrompt = useStoreActions<StoreModel>(
+    (actions) => actions?.promptModel?.setPrompt
   );
   const { setConversation, addMessage } = useStoreActions<StoreModel>(
     (actions) => actions.conversationModel
   );
 
-  const [inputValue, setInputValue] = useState(
-    pathname.startsWith('/overview') ? promptData.question.trim() : ''
-  );
-  const textareaRef = useRef<HTMLTextAreaElement>(null);
-  const debouncedInput = useDebounce(inputValue, 1000);
-
-  const handleBlur = () => {
-    if (textareaRef.current) {
-      setTimeout(() => {
-        if (textareaRef.current) {
-          textareaRef.current.focus();
-          textareaRef.current.setSelectionRange(
-            textareaRef.current.value.length,
-            textareaRef.current.value.length
-          );
-        }
-      }, 1000);
-    }
-  };
-
+  const [inputValue, setInputValue] = useState('');
   useEffect(() => {
-    if (textareaRef.current) {
-      textareaRef.current.focus();
-      textareaRef.current.setSelectionRange(
-        textareaRef.current.value.length,
-        textareaRef.current.value.length
-      );
-    }
-  }, [pathname]);
-
-  useEffect(() => {
-    if (prompt?.question) {
-      setInputValue(prompt?.question.trim());
-    }
+    setInputValue(prompt?.question || '');
   }, [prompt]);
-
-  useEffect(() => {
-    if (debouncedInput && promptData.loader) {
-      setPrompt({ question: debouncedInput });
-      setInputValue(debouncedInput);
-    }
-  }, [debouncedInput]);
 
   const { mutate } = useMutation({
     isToaster: false,
@@ -94,7 +56,8 @@ const TextArea = ({
   });
 
   const handleSubmit = () => {
-    if (inputValue.length > 0) {
+    setPrompt({ question: inputValue });
+    if (inputValue) {
       addMessage({
         role: 'user',
         content: inputValue,
@@ -108,12 +71,10 @@ const TextArea = ({
         userId: user.id,
       });
       setInputValue('');
-      setPrompt({ question: '' });
     }
   };
-
   return (
-    <div className="relative mt-10 flex w-full flex-col items-end justify-between rounded-xl bg-white p-2 shadow-lg xl:mb-5 xl:flex-row">
+    <div className="relative mt-10 flex w-full items-end justify-between rounded-xl bg-white shadow-lg xl:mb-5">
       <div className="flex w-full items-end">
         <Button
           className={`${
@@ -123,35 +84,29 @@ const TextArea = ({
           <Addicon />
         </Button>
 
-        <textarea
-          ref={textareaRef}
-          onBlur={handleBlur}
-          id="custom-textarea"
+        <Textarea
           placeholder="Type '/' for commands"
-          className="!min-h-[90px] w-full resize-none !rounded-none !p-0 !pl-[30px] !pr-[42px] text-black shadow-none scrollbar-hide focus:!border-none focus:!outline-none focus:!ring-0"
+          className="max-w-full resize-none text-black shadow-none"
           value={inputValue}
           onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) =>
             setInputValue(e.target.value)
           }
-          onKeyDown={(e) => {
-            if (
-              e.key === 'Enter' &&
-              !e.shiftKey &&
-              !promptData?.loader &&
-              inputValue.length > 0
-            ) {
-              e.preventDefault();
-              handleSubmit();
-            }
+          maxRows={6}
+          classNames={{
+            base: `!p-0 !bg-transparent ${classNames?.base}`,
+            innerWrapper: `flex items-center ${classNames?.innerWrapper}`,
+            input: `scrollbar-hide  !rounded-none !min-h-[60px] ${classNames?.input}`,
+            inputWrapper: `!pl-[30px] !pr-[42px] bg-transparent data-[hover=true]:bg-transparent data-[focus=true]:!bg-transparent shadow-none ${classNames?.inputWrapper}`,
           }}
+          {...props}
         />
       </div>
 
       <Button
-        disabled={promptData.loader}
+        disabled={!inputValue && promptData.loader}
         className={`h-10 w-14 ${
-          promptData.loader ? 'cursor-not-allowed bg-opacity-30' : ''
-        } min-w-fit rounded-md bg-custom-gradient px-3 py-2.5 text-white group-hover:bg-custom-white`}
+          !inputValue ? 'cursor-not-allowed bg-opacity-30' : ''
+        } absolute bottom-1 right-2.5 z-[5] min-w-fit rounded-md bg-custom-gradient px-3 py-2.5 text-white group-hover:bg-custom-white`}
         onClick={handleSubmit}
       >
         <span className="leading-none">
