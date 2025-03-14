7ea5d63f46f94982038a27b9dae3fcc17da0fae0
Syed-Abdullah-Expertizo <abdullah.naseer@expertizo.pk>
2025-03-14 10:55:05 +0500

    refactor: regex and remove unwanted libraries

--- client/src/constants/regex.ts
+++ client/src/constants/regex.ts
@@ -3,10 +3,6 @@ export const PHONE_REGEX = /^[0-9]{10,15}$/;
 export const PASSWORD_REGEX = /^(?=.*\d)(?=.*[A-Z])(?=.*\W).{8,}$/;
 export const ONLY_ALPHABET_REGEX = /^[a-zA-Z\s]+$/;
 export const GITHUB_TOKEN_REGEX = [
-  /^ghp_[a-zA-Z0-9]{36}$/,
-  /^github_pat_[a-zA-Z0-9]{22}_[a-zA-Z0-9]{59}$/,
-  /^gho_[a-zA-Z0-9]{36}$/,
-  /^ghu_[a-zA-Z0-9]{36}$/,
-  /^ghs_[a-zA-Z0-9]{36}$/,
-  /^ghr_[a-zA-Z0-9]{36}$/,
+  /^github_pat_\w{22}_\w{59}$/,
+  /^gh[posru]_\w{36}$/
 ];
