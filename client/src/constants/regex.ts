f72255cf0ee6ad90c7063a5a524e4d497a29da89
Syed-Abdullah-Expertizo <abdullah.naseer@expertizo.pk>
2025-03-14 11:03:37 +0500

    fix: build errors

--- client/src/constants/regex.ts
+++ client/src/constants/regex.ts
@@ -4,5 +4,5 @@ export const PASSWORD_REGEX = /^(?=.*\d)(?=.*[A-Z])(?=.*\W).{8,}$/;
 export const ONLY_ALPHABET_REGEX = /^[a-zA-Z\s]+$/;
 export const GITHUB_TOKEN_REGEX = [
   /^github_pat_\w{22}_\w{59}$/,
-  /^gh[posru]_\w{36}$/
+  /^gh[posru]_\w{36}$/,
 ];
