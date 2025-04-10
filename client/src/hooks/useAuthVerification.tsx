b6beb108d93d09771a9437c831b2f89fa93c2b6f
Mohammad Kashif Sulaiman <m.kqagraphix@gmail.com>
2025-04-10 13:48:19 +0500

    feat: implement Google authentication hook and middleware integration

--- client/src/hooks/useAuthVerification.tsx
+++ client/src/hooks/useAuthVerification.tsx
@@ -1,15 +1,30 @@
 import { useQuery } from './useQuery';
 import { usePathname } from 'next/navigation';
 import { ApiUrl } from '@/constants/apiUrl';
+import Cookies from 'js-cookie';
+import { useEffect } from 'react';
+import { StoreModel } from '@/redux/model';
+import { useStoreActions } from 'easy-peasy';
 
 export const useAuthVerification = () => {
   const pathName = usePathname();
+  const jwtToken = Cookies.get('jwtToken');
+  const userAction = useStoreActions<StoreModel>((actions) => actions.userObj.add);
 
-  useQuery<any>({
+  const { data, isLoading } = useQuery<any>({
     queryKey: [pathName],
 
     url: ApiUrl?.VERIFY_TOKEN,
 
     showToast: true,
+    enabled: !!jwtToken,
   });
+  useEffect(() => {
+    if (isLoading) return;
+    if (data) {
+      userAction(data);
+    } else {
+      Cookies.remove('jwtToken');
+    }
+  }, [data, isLoading])
 };
---
+++ client/src/hooks/useGoogleAuth.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useRef } from 'react';
+import { useRouter, useSearchParams, usePathname } from 'next/navigation';
+import Cookies from 'js-cookie';
+import { showToaster } from '@/components/Toaster';
+
+export const useGoogleAuth = () => {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const token = searchParams.get('token');
+  const hasRun = useRef(false);
+  const router = useRouter();
+ 
+  useEffect(() => {
+    if (hasRun.current) return; // Prevent re-running the logic if already executed
+    if (pathname.endsWith('main') && token) {
+      hasRun.current = true;
+      Cookies.set('jwtToken', token);
+      const current = new URLSearchParams(searchParams.toString());
+      current.delete('token');
+      router.push(`${pathname}${current.toString() ? `?${current}` : ''}`);
+      showToaster(`Logged in with Google successfully!`, 'success');
+    }
+  }, [token]);
+
+
+};
